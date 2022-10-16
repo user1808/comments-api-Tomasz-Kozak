187ac9914cd299dac13d8b12300f2186fdce410b
user1808 <tomasz.kozak04@gmail.com>
2022-10-16 22:27:23 +0200

    add basic db connection

--- app.js
+++ app.js
@@ -14,5 +14,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Importing routes from dir ./routes
 app.use('/', routes);
 console.log(`Listening: http://localhost:${process.env.APP_PORT || '3000'}/`);
+console.log(process.env.DB_HOST)
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+})
+
+connection.connect()
+
+connection.query('SELECT * FROM comments', (err, rows, fields) => {
+  if (err) {
+    console.log('Error')
+    throw err
+  }
+
+  console.log('ALright')
+})
+
+connection.end()
+
 
 module.exports = app;
