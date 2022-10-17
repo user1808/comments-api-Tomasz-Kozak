be86d9cefe31540473eede2038a88a6fd9f36a4a
user1808 <tomasz.kozak04@gmail.com>
2022-10-17 12:24:10 +0200

    remove connection

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import mysql from 'mysql';
 
 import routes from './routes';
 
@@ -15,26 +14,5 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Importing routes from dir ./routes
 app.use('/', routes);
 console.log(`Listening: http://localhost:${process.env.APP_PORT || '3000'}/`);
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-})
-
-connection.connect()
-
-connection.query('SELECT * FROM comments', (err, rows, fields) => {
-  if (err) {
-    console.log('Error')
-    throw err
-  }
-
-  console.log('ALright')
-})
-
-connection.end()
-
 
 module.exports = app;
