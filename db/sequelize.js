9ad0b672bc17aa717ac3fab8c68168d335e1f507
user1808 <tomasz.kozak04@gmail.com>
2022-10-17 15:31:28 +0200

    remove mysql2

--- db/sequelize.js
+++ db/sequelize.js
@@ -3,8 +3,6 @@ import { Sequelize } from 'sequelize';
 import { Comment } from '../model/comment'
 import { COMMENTS_TABLE_NAME } from '../utils/constants';
 
-// TODO: Pozbyć się tego mysql2
-
 const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
     host: config.db.host,
     dialect: config.db.dialect,
