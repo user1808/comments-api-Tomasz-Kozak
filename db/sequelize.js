8d328f418fb040705fdfe8a976ce5c4299a53dc2
user1808 <tomasz.kozak04@gmail.com>
2022-10-17 15:38:09 +0200

    mysql -> mysql2

--- db/sequelize.js
+++ db/sequelize.js
@@ -2,10 +2,12 @@ import { config } from './config';
 import { Sequelize } from 'sequelize';
 import { Comment } from '../model/comment'
 import { COMMENTS_TABLE_NAME } from '../utils/constants';
+import mysql2 from 'mysql2';
 
 const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
     host: config.db.host,
     dialect: config.db.dialect,
+    dialectModule: mysql2,
 })
 
 sequelize.define(COMMENTS_TABLE_NAME, Comment)
